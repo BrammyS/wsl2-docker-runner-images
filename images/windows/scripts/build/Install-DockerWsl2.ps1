b10ed2dd5584a992fefd0763d4dea96172b15344
Bram Esendam <bram@maryor.nl>
2025-05-06 11:49:11 +0200

    Docker repo not being added properly to WSL2 ubuntu

--- images/windows/scripts/build/Install-DockerWsl2.ps1
+++ images/windows/scripts/build/Install-DockerWsl2.ps1
@@ -1,3 +1,4 @@
+Write-Host "Installing Docker for WSL2"
 wsl --version
 wsl --install Ubuntu
 
@@ -5,30 +6,34 @@ wsl --install Ubuntu
 wsl -d Ubuntu --exec dbus-launch true
 wsl sudo apt-get update
 
-# Add Docker repository to apt sources
+Write-Host "Adding Docker repository to apt sources"
 wsl sudo apt-get install ca-certificates curl
 wsl sudo install -m 0755 -d /etc/apt/keyrings
 wsl sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 wsl sudo chmod a+r /etc/apt/keyrings/docker.asc
-wsl sudo bash -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo ${UBUNTU_CODENAME:-$VERSION_CODENAME}) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
-wsl sudo apt-get update
-Start-Sleep 10
+wsl -- sudo bash -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo ${UBUNTU_CODENAME:-$VERSION_CODENAME}) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
 
-# Install Docker Engine, CLI, and Containerd
+Write-Host "Docker repository added to apt sources"
+Start-Sleep 5
+wsl sudo cat /etc/apt/sources.list.d/docker.list
+wsl sudo apt-get update
+
+Write-Host "Installing Docker Engine, CLI, and Containerd"
+Start-Sleep 5
 wsl sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 wsl docker info
 
-# Expose Docker daemon to Windows
+Write-Host "Exposing Docker daemon to Windows"
 wsl bash -c "sudo sed -i 's|fd://|tcp://127.0.0.1:2375|' /usr/lib/systemd/system/docker.service"
 wsl sudo systemctl daemon-reload
 choco install docker-cli
 
-# Use WSL2 docker daemon, set environment variables for Docker CLI
+Write-Host "Setting environment variables for Docker CLI"
 [Environment]::SetEnvironmentVariable('DOCKER_HOST', 'tcp://localhost:2375', 'Process')
 [Environment]::SetEnvironmentVariable('DOCKER_HOST', 'tcp://localhost:2375', 'User')
 [Environment]::SetEnvironmentVariable('DOCKER_HOST', 'tcp://localhost:2375', 'Machine')
 
-# Verify Docker CLI is using WSL2 daemon
+Write-Host "Verifying Docker CLI is using WSL2 daemon"
 docker info
 docker run --name test-hello-world hello-world:linux
 docker rm test-hello-world
