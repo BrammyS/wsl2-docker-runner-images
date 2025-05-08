11bbd2d41bca92751fa468ae4b07fb8a3e7e5e13
Bram Esendam <bram@maryor.nl>
2025-05-08 12:42:52 +0200

    Exposed daemon is not being applied

--- images/windows/scripts/build/Install-DockerWsl2.ps1
+++ images/windows/scripts/build/Install-DockerWsl2.ps1
@@ -26,6 +26,8 @@ wsl docker info
 Write-Host "Exposing Docker daemon to Windows"
 wsl bash -c "sudo sed -i 's|fd://|tcp://127.0.0.1:2375|' /usr/lib/systemd/system/docker.service"
 wsl sudo systemctl daemon-reload
+wsl sudo systemctl restart docker
+wsl sudo systemctl status docker
 choco install docker-cli
 
 Write-Host "Setting environment variables for Docker CLI"
